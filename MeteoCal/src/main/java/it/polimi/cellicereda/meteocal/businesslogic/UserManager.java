a03b49f1834ad9ce06ba8eeecf787f76dabc8dca
Stefano Cereda <cereda.ste@gmail.com>
2014-12-27 11:34:44 +0100

    Added the user manager

---
+++ MeteoCal/src/main/java/it/polimi/cellicereda/meteocal/businesslogic/UserManager.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.cellicereda.meteocal.businesslogic;
+
+import it.polimi.cellicereda.meteocal.entities.Group;
+import it.polimi.cellicereda.meteocal.entities.User;
+import java.security.Principal;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author stefano
+ */
+@Stateless
+public class UserManager {
+    @PersistenceContext
+    EntityManager em;
+    
+    @Inject
+    Principal principal;
+
+    public void save(User user) {
+        user.setGroupName(Group.USERS);
+        em.persist(user);
+    }
+
+    public void unregister() {
+        em.remove(getLoggedUser());
+    }
+
+    public User getLoggedUser() {
+        return em.find(User.class, principal.getName());
+    }
+}
