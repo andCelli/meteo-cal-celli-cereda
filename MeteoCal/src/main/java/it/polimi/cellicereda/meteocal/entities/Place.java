ada726b4a38931da96e79e1d276356c2a196f499
Stefano Cereda <cereda.ste@gmail.com>
2015-01-03 12:48:57 +0100

    Completed the Place entity

--- MeteoCal/src/main/java/it/polimi/cellicereda/meteocal/entities/Place.java
+++ MeteoCal/src/main/java/it/polimi/cellicereda/meteocal/entities/Place.java
@@ -46,7 +46,7 @@ public class Place implements Serializable {
      */
     @NotNull(message = "May not be empty")
     private Double longitude;
-    
+
     /**
      * The location country as referenced by the meteo service
      */
@@ -60,8 +60,38 @@ public class Place implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-    
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     @Override
     public int hashCode() {
