f0c5a88bc5ba31681961538ae3e2eaecb08a8d02
Andrea_Celli <andrea.celli@mail.polimi.it>
2015-01-06 16:55:23 +0100

    updates in che ScheduleBean and small fix in the CalendarManager

--- MeteoCal/src/main/java/it/polimi/cellicereda/meteocal/gui/ScheduleBean.java
+++ MeteoCal/src/main/java/it/polimi/cellicereda/meteocal/gui/ScheduleBean.java
@@ -5,8 +5,15 @@
  */
 package it.polimi.cellicereda.meteocal.gui;
 
+import it.polimi.cellicereda.meteocal.businesslogic.CalendarManager;
+import it.polimi.cellicereda.meteocal.businesslogic.UserProfileManager;
+import it.polimi.cellicereda.meteocal.entities.User;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
 import org.primefaces.model.ScheduleModel;
 
 /**
@@ -18,10 +25,19 @@ import org.primefaces.model.ScheduleModel;
 @ViewScoped
 public class ScheduleBean {
     
+    @EJB
+    CalendarManager calendarManager;
+    @EJB
+    UserProfileManager userProfileManager;
+    
+    //this will contain the list of events to be displayed
     private ScheduleModel model;
-            
+    private User currentUser;
+    
     public ScheduleBean(){
-        
+        currentUser=userProfileManager.getLoggedUser();
+
+        model=new DefaultScheduleModel((List<ScheduleEvent>) calendarManager.getEventsByParticipant(currentUser));
     }        
      
     
