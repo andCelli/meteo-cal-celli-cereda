c08601510be9705f62f35fd44bf61ed11de2ea75
Andrea_Celli <andrea.celli@mail.polimi.it>
2015-01-09 12:12:24 +0100

    schedule bean and xhtml update

--- MeteoCal/src/main/java/it/polimi/cellicereda/meteocal/gui/ScheduleBean.java
+++ MeteoCal/src/main/java/it/polimi/cellicereda/meteocal/gui/ScheduleBean.java
@@ -44,9 +44,21 @@ public class ScheduleBean implements Serializable{
     private ScheduleModel model;
     private User currentUser;
     private Event event=new Event();
+    /*
+    these are the attributes that are used to decouple the gui from the entities
+    we need only the attributes to be displayed and set by the user
+    */
+    private String title;
+    private String description;
+    private Date startingDate;
+    private Date endingDate;
+    private String location;
+    private boolean isPublic;
+    private boolean allDay;
+    
     
     @PostConstruct
-    public void init(){
+    public void init(){     
         currentUser=userProfileManager.getLoggedUser();
         //find all the events in which the user will partecipate 
         model=new DefaultScheduleModel((List<ScheduleEvent>) calendarManager.getEventsByParticipant(currentUser));
@@ -55,16 +67,96 @@ public class ScheduleBean implements Serializable{
     public ScheduleModel getModel() {
         return model; 
     }
-    
+    public void setModel(ScheduleModel model){
+        this.model=model;
+    }
     public Event getEvent(){
         return event;
     }
-    
     public void setEvent(Event event){
         this.event = event; 
     }
+    private void addMessage(FacesMessage message) {
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    } 
+    /*
+    This method checks whether the user is modifying an existing event or he's creating
+    a new one. In the first case the method updates event info. In the second it creates
+    a new event instance in the db.
+    */
+    public void saveEvent(){
+        //check
+        if(event.getTitle().equals(null)){
+            //set info (va bene farlo prima che venga aggiunto??)
+            calendarManager.changeEventTitle(event, title);
+            calendarManager.changeEventDescription(event, description);
+            calendarManager.changeEventTiming(event, startingDate, endingDate);
+            //correggere
+            calendarManager.changeEventLocation(event, endingDate);
+            event.setPublicEvent(isPublic);
+            event.setIsAllDay(allDay);
+            event.setCreator(currentUser);
+            calendarManager.save(event);
+        }else{
+            //@TODO refactor
+            calendarManager.changeEventTitle(event, title);
+            calendarManager.changeEventDescription(event, description);
+            calendarManager.changeEventTiming(event, startingDate, endingDate);
+            //correggere
+            calendarManager.changeEventLocation(event, endingDate);
+            event.setPublicEvent(isPublic);
+            event.setIsAllDay(allDay);
+        }
+        model.addEvent(event);
+        event=new Event();
+    }
     
-    public void addEvent() {
+    
+    
+    
+    
+    
+    
+    
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description=description;
+    }public Date getStartingDate(){
+        return startingDate;
+    }
+    public void setStartingDate(Date startingDate){
+        this.startingDate=startingDate;
+    }public Date getEndingDate(){
+        return endingDate;
+    }
+    public void setEndingDate(Date endingDate){
+        this.endingDate=endingDate;
+    }public String getLocation(){
+        return location;
+    }
+    public void setLocation(String location){
+        this.location=location;
+    }public boolean getIsPublic(){
+        return isPublic;
+    }
+    public void setIsPublic(boolean isPublic){
+        this.isPublic=isPublic;
+    }
+    public boolean getAllDay(){
+        return allDay;
+    }
+    public void setAllDay(boolean allDay){
+        this.allDay=allDay;
+    }
+    /*public void addEvent(){
         //check if the model already contains the event
         //if(modelContains(event)){
           //   model.addEvent(event);
@@ -99,7 +191,7 @@ public class ScheduleBean implements Serializable{
              //event.setStartingDate(date);
     }
     
-     //aggiungere l'aggiornamento dell'evento nel db
+    /* //aggiungere l'aggiornamento dell'evento nel db
      public void onEventMove(ScheduleEntryMoveEvent event) {  
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", 
                 "Day delta: " + event.getDayDelta() + ", Minute delta: " + event.getMinuteDelta());
@@ -112,9 +204,5 @@ public class ScheduleBean implements Serializable{
                 "Event resized", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
          
         addMessage(message);
-    } 
-     
-    private void addMessage(FacesMessage message) {
-        FacesContext.getCurrentInstance().addMessage(null, message);
-    } 
+    } */
 }
