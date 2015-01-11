88dc82b5f886944fb3334d2f6b0911c73c7b11e0
Stefano Cereda <cereda.ste@gmail.com>
2015-01-11 12:32:58 +0100

    Bugfix in the forecast downloader and related test. The forecast download is correctly working

--- MeteoCal/src/main/java/it/polimi/cellicereda/meteocal/businesslogic/ForecastManager.java
+++ MeteoCal/src/main/java/it/polimi/cellicereda/meteocal/businesslogic/ForecastManager.java
@@ -9,10 +9,12 @@ import it.polimi.cellicereda.meteocal.entities.Forecast;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.EJB;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.primefaces.json.JSONArray;
@@ -27,8 +29,15 @@ public class ForecastManager {
 
     @PersistenceContext
     EntityManager em;
-    
-    private String downloadForecastsByCityID(int cityID) {
+
+    @EJB
+    LocationManager lm;
+
+    /**
+     * Download from openweathermap the forecasts for the given city, the
+     * forecast have a time span of 5 days with data every 3 hours
+     */
+    private String downloadForecastsByCityID(Long cityID) {
         try {
             String url = "http://api.openweathermap.org/data/2.5/forecast?id=" + cityID;
             return URLConnectionReader.getText(url);
@@ -37,50 +46,48 @@ public class ForecastManager {
             return null;
         }
     }
-    
-    public List<Forecast> getNewForecastsByCityID(int cityID) {
+
+    /**
+     * Given a cityID this method queries openweathermap to obtain the updated
+     * forecast, which are then returned as a list
+     */
+    public List<Forecast> getNewForecastsByCityID(Long cityID) {
         try {
             JSONObject entireForecast = new JSONObject(downloadForecastsByCityID(cityID));
             JSONArray forecastList = entireForecast.getJSONArray("list");
 
-            //TODO SET THE MAKING TIME ("dt":1420869600)
+            //compute the making time
+            Date now = new Date();
+
             for (int i = 0; i < forecastList.length(); i++) {
                 JSONObject forecastJson = forecastList.getJSONObject(i);
-                
+
                 Forecast forecast = new Forecast();
 
-                //TODO SET THE PLACE
-                
-                //SET THE STARTING/ENDING VALIDITY TIME 
-                try {
-                    String starting = forecastJson.getString("dt_txt");
-                    DateFormat format = new SimpleDateFormat("YYYY-MM-DD HH:MM:SS");
-                    Date startingValidity = format.parse(starting);
+                //SET THE PLACE
+                forecast.setPlace(lm.getPlaceByID(cityID));
 
-                    //the forecasts have a time coverage of three hours
-                    Date endingValidity = new Date(startingValidity.getTime() + (3 * 60 * 60 * 1000));
-                    
-                    forecast.setStartingValidity(startingValidity);
-                    forecast.setEndingValidity(endingValidity);
-                } catch (ParseException ex) {
-                    Logger.getLogger(ForecastManager.class.getName()).log(Level.SEVERE, "Unable to parse starting validity time", ex);
-                    
-                }
+                //TODO:SET THE STARTING/ENDING VALIDITY TIME (time coverage = 3 hours)
+                Date starting = new Date(forecastJson.getLong("dt") * 1000);
+                Date ending = new Date(starting.getTime() + (3 * 60 * 60 * 1000));
+                forecast.setStartingValidity(starting);
+                forecast.setEndingValidity(ending);
 
-                //TODO SAVE THE MAKING TIME (COMPUTED BEFORE)
-                
+                //SAVE THE MAKING TIME (computed before and equals for all the forecasts)
+                forecast.setMakingTime(now);
+
                 //SET THE WEATHER ID
-                forecast.setWeatherId(forecastJson.getJSONObject("weather").getInt("id"));
+                forecast.setWeatherId(forecastJson.getJSONArray("weather").getJSONObject(0).getInt("id"));
 
                 //PERSIST THE FORECAST
                 em.persist(forecast);
             }
-            
+
         } catch (JSONException ex) {
             Logger.getLogger(ForecastManager.class.getName()).log(Level.SEVERE, "Unable to download forecasts", ex);
             return null;
         }
-        
+
         return null;
     }
 }
---
+++ MeteoCal/src/test/java/it/polimi/cellicereda/meteocal/businesslogic/ForecastManagerTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.cellicereda.meteocal.businesslogic;
+
+import it.polimi.cellicereda.meteocal.entities.Place;
+import javax.persistence.EntityManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Matchers.any;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ *
+ * @author stefano
+ */
+public class ForecastManagerTest {
+
+    private ForecastManager fm;
+
+    @Before
+    public void setUp() {
+        fm = new ForecastManager();
+        fm.em = mock(EntityManager.class);
+        fm.lm = mock(LocationManager.class);
+
+        //    forecast.setPlace(lm.getPlaceByID(cityID));
+        //create a place entity
+        Place moscow = new Place();
+        moscow.setId((long) 524901);
+        moscow.setName("Moscow");
+        moscow.setLongitude(37.615555);
+        moscow.setLatitude(55.75222);
+
+        //and return it when the forecast manager asks it to the location manager
+        Mockito.when(fm.lm.getPlaceByID((long) 524901)).thenReturn(moscow);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void getNewForecastsByCityID() {
+        fm.getNewForecastsByCityID((long) 524901);
+        verify(fm.em, atLeast(10)).persist(any());
+    }
+
+}
